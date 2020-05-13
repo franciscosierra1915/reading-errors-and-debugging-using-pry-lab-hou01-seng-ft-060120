e1929f19654fca4a61e855eae28caa4157735a89
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-13 17:26:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,4 @@
 # don't forget to add: require 'pry'
-require 'pry'
 def get_user_input
   gets.chomp
 end
@@ -22,4 +21,3 @@ def runner
   prompt_user
   selection(get_user_input)
 end
- binding.pry
