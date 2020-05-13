ba98de52720fbae35070cc1513c02a8db5b085c0
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-13 17:35:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     10 * "s" + string
@@ -7,3 +7,4 @@ def snake_it_up(string)
     string
   end
 end
+binding.pry
